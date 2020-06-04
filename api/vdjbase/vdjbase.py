56a25e760724f0ca6ff21d1128ce3f83d7ebd4ca
William Lees <william@lees.org.uk>
2020-06-04 16:11:17 +0100

    Fixes

--- api/vdjbase/vdjbase.py
+++ api/vdjbase/vdjbase.py
@@ -9,10 +9,11 @@ import json
 from sqlalchemy_filters import apply_filters
 from werkzeug.exceptions import BadRequest
 from datetime import datetime
+import decimal
 
 
 from app import vdjbase_dbs
-from db.vdjbase_model import Sample, GenoDetection, Patient, SeqProtocol, Study, TissuePro, HaplotypesFile, SamplesHaplotype
+from db.vdjbase_model import Sample, GenoDetection, Patient, SeqProtocol, Study, TissuePro, HaplotypesFile, SamplesHaplotype, Allele
 
 # Return SqlAlchemy row as a dict, using correct column names
 def object_as_dict(obj):
@@ -258,13 +259,13 @@ class SamplesApi(Resource):
 
         for spec in sort_specs:
             if spec['field'] in valid_filters.keys():
-                ret = sorted(ret, key=lambda x : (x[spec['field']] is None,  x[spec['field']]), reverse=(spec['order'] == 'desc'))
+                ret = sorted(ret, key=lambda x : ((x[spec['field']] is None or x[spec['field']] == ''),  x[spec['field']]), reverse=(spec['order'] == 'desc'))
 
         if args['page_size']:
             first = (args['page_number']) * args['page_size']
             ret = ret[first : first + args['page_size']]
 
-        if 'haplotypes' in required_cols:
+        if 'haplotypes' in required_cols:                                   # BUG - this won't work with multiple datasets
             haplotypes = session.query(Sample.name, func.group_concat(HaplotypesFile.by_gene_s))
 
             for r in ret:
@@ -273,6 +274,141 @@ class SamplesApi(Resource):
                     r['haplotypes'] = h[1]
                 else:
                     r['haplotypes'] = ''
+
+        return {
+            'samples': ret,
+            'uniques': uniques,
+            'total_items': total_size,
+            'page_size': args['page_size'],
+            'pages': ceil((total_size*1.0)/args['page_size'])
+        }
+
+valid_sequence_cols = {
+    'name': {'model': 'Allele', 'field': Allele.name},
+    'seq': {'model': 'Allele', 'field': Allele.seq, 'no_uniques': True},
+    'seq_len': {'model': 'Allele', 'field': Allele.seq_len},
+    'similar': {'model': 'Allele', 'field': Allele.similar},
+    'appears': {'model': 'Allele', 'field': Allele.appears},
+    'is_single_allele': {'model': 'Allele', 'field': Allele.is_single_allele},
+    'low_confidence': {'model': 'Allele', 'field': Allele.low_confidence},
+    'novel': {'model': 'Allele', 'field': Allele.novel},
+    'max_kdiff': {'model': 'Allele', 'field': Allele.max_kdiff},
+}
+
+@ns.route('/sequences/<string:species>/<string:dataset>')
+class SequencesApi(Resource):
+    @api.expect(filter_arguments, validate=True)
+    def get(self, species, dataset):
+        """ Returns the list of sequences in the selected dataset """
+
+        if species not in vdjbase_dbs or set(dataset.split(',')).difference(set(vdjbase_dbs[species])):
+            return list()
+
+        args = filter_arguments.parse_args(request)
+        if (args['page_size'] and args['page_size'] <= 0) or (args['page_number'] and args['page_number'] < 0):
+            return list()
+
+        if args['cols'] is None or not args['cols']:
+            return list()
+
+        required_cols = json.loads(args['cols'])
+
+        for col in required_cols:
+            if col not in valid_sequence_cols.keys():
+                print('bad column in request: %s' % col)
+                return list(), 404
+
+        hap_filters = None
+
+        filter_spec = []
+        if args['filter']:
+            for f in json.loads(args['filter']):
+                try:
+                    if f['field'] != 'haplotypes':
+                        f['model'] = valid_sequence_cols[f['field']]['model']
+                        if 'fieldname' in valid_sequence_cols[f['field']]:
+                            f['field'] = valid_sequence_cols[f['field']]['fieldname']
+                        if '(blank)' in f['value']:
+                            f['value'].append('')
+                        filter_spec.append(f)
+                    else:
+                        hap_filters = f
+                except:
+                    raise BadRequest('Bad filter string %s' % args['filter'])
+
+        samples = []
+
+        for dset in dataset.split(','):
+            session = vdjbase_dbs[species][dset].session
+
+            attribute_query = []
+
+            for col in required_cols:
+                if valid_sequence_cols[col]['field'] is not None:
+                    attribute_query.append(valid_sequence_cols[col]['field'])
+
+            query = session.query(*attribute_query)
+            samples.extend(query.all())
+
+        uniques = {}
+        for f in required_cols:
+            uniques[f] = []
+
+        for s in samples:
+            for f in required_cols:
+                if valid_sequence_cols[f]['field'] is not None and 'no_uniques' not in valid_sequence_cols[f]:
+                    el = getattr(s, f)
+                    if isinstance(el, datetime):
+                        el = el.date().isoformat()
+                    elif isinstance(el, decimal.Decimal):
+                        el = '%0.2f' % el
+                    elif isinstance(el, str) and len(el) == 0:
+                        el = '(blank)'
+                    if el not in uniques[f]:
+                        uniques[f].append(el)
+
+        for f in required_cols:
+            try:
+                uniques[f].sort(key=lambda x: (x is None or x == '', x))
+            except:
+                pass
+
+        ret = []
+
+        for dset in dataset.split(','):
+            session = vdjbase_dbs[species][dset].session
+
+            attribute_query = []
+
+            for col in required_cols:
+                if valid_sequence_cols[col]['field'] is not None:
+                    attribute_query.append(valid_sequence_cols[col]['field'])
+
+            query = session.query(*attribute_query)
+            query = apply_filters(query, filter_spec)
+            res = query.all()
+
+            for r in res:
+                s = r._asdict()
+                for k, v in s.items():
+                    if isinstance(v, datetime):
+                        s[k] = v.date().isoformat()
+                    elif isinstance(v, decimal.Decimal):
+                        s[k] = '%0.2f' % v
+                s['dataset'] = dset
+                ret.append(s)
+
+        total_size = len(ret)
+
+        sort_specs = json.loads(args['sort_by']) if ('sort_by' in args and args['sort_by'] != None)  else [{'field': 'name', 'order': 'asc'}]
+
+        for spec in sort_specs:
+            if spec['field'] in valid_sequence_cols.keys():
+                ret = sorted(ret, key=lambda x : ((x[spec['field']] is None or x[spec['field']] == ''),  x[spec['field']]), reverse=(spec['order'] == 'desc'))
+
+        if args['page_size']:
+            first = (args['page_number']) * args['page_size']
+            ret = ret[first : first + args['page_size']]
 
         return {
             'samples': ret,
