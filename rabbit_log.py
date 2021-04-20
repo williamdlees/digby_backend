0c33345a1fb778167199eba04bfe05a2c4d9862e
William Lees <william@lees.org.uk>
2021-04-20 17:04:58 +0100

    FIx rabbit heartbeat issue

--- rabbit_log.py
+++ rabbit_log.py
@@ -10,27 +10,26 @@ import pika
 class FlaskRabbitLogHandler(logging.Handler):
     def __init__(self, url, appname, *args, **kwargs):
         super(FlaskRabbitLogHandler, self).__init__(*args, **kwargs)
-        try:
-            self.url = url
-            self.appname = appname
-            self.connection = pika.BlockingConnection(
-                    pika.URLParameters(self.url))
-            self.channel = self.connection.channel()
-            self.channel.queue_declare(queue='mail')
-        except:
-            print('Exception initialising RabbitMQ')
+        self.url = url
+        self.appname = appname
 
     def __del__(self):
-        if self.connection:
-            self.connection.close()
+        pass
 
     def emit(self, record):
-        try:
-            self.channel.basic_publish(
-                exchange='',
-                routing_key='mail',
-                body=self.format(record),
-                properties=pika.BasicProperties(type=record.levelname, app_id=self.appname)
-            )
-        except:
-            print('Exception logging error to RabbitMQ')
+        # set up the connection here, because we only expect the occasional message and
+        # blocking connections are bad at maintaining the heartbeat
+        self.connection = pika.BlockingConnection(
+                pika.URLParameters(self.url))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='mail')
+
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='mail',
+            body=self.format(record),
+            properties=pika.BasicProperties(type=record.levelname, app_id=self.appname)
+        )
+
+        if self.connection:
+            self.connection.close()
