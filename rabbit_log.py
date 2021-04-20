061b6628696cbc1b4d3298a6246b9645fa8c99cf
William Lees <william@lees.org.uk>
2021-04-20 10:55:24 +0100

    Catch exceptions to avoid recursion

--- rabbit_log.py
+++ rabbit_log.py
@@ -10,21 +10,27 @@ import pika
 class FlaskRabbitLogHandler(logging.Handler):
     def __init__(self, url, appname, *args, **kwargs):
         super(FlaskRabbitLogHandler, self).__init__(*args, **kwargs)
-        self.url = url
-        self.appname = appname
-        self.connection = pika.BlockingConnection(
-                pika.URLParameters(self.url))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='mail')
+        try:
+            self.url = url
+            self.appname = appname
+            self.connection = pika.BlockingConnection(
+                    pika.URLParameters(self.url))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue='mail')
+        except:
+            print('Exception initialising RabbitMQ')
 
     def __del__(self):
         if self.connection:
             self.connection.close()
 
     def emit(self, record):
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='mail',
-            body=self.format(record),
-            properties=pika.BasicProperties(type=record.levelname, app_id=self.appname)
-        )
+        try:
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='mail',
+                body=self.format(record),
+                properties=pika.BasicProperties(type=record.levelname, app_id=self.appname)
+            )
+        except:
+            print('Exception logging error to RabbitMQ')
