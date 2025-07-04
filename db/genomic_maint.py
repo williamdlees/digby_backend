250612cfcc8bd30f7b888955f4665d205499ed1b
William Lees <william@lees.org.uk>
2025-07-04 14:45:15 +0100

    FIxes for null fields

--- db/genomic_maint.py
+++ db/genomic_maint.py
@@ -1,4 +1,5 @@
 # Create database and associated files for a single genomic dataset
+from datetime import date
 import json
 
 from receptor_utils import simple_bio_seq as simple
@@ -9,11 +10,11 @@ from sqlalchemy.pool import NullPool
 import os
 import yaml
 
-from db.genomic_ref import update_genomic_ref, read_gene_order, add_alias_sets
+from db.genomic_ref import update_genomic_ref, read_gene_order
 from db.genomic_airr_model import Sample, Study, Patient, SeqProtocol, TissuePro, DataPro
 from db.genomic_db import Base, RefSeq
 from db.genomic_db_functions import save_genomic_dataset_details, save_genomic_ref_seq, calculate_appearances, calculate_max_cov_sample
-from db.igenotyper import process_genomic_record, add_gene_level_features, add_ighc_gene_level_features
+from db.igenotyper import process_genomic_record, add_gene_level_features
 from db.bed_file import read_bed_files
 from db.source_details import db_source_details
 
@@ -44,10 +45,10 @@ def create_dataset(species, dataset):
         for val in ('Reference_set_version', 'Reference_sets'):
             if val not in study_data:
                 raise ImportException(f'Error - {val} is missing from the study metadata.')
-
+            
         read_gene_order(session, dataset_dir)
         for file in study_data['Reference_sets']:
-            update_genomic_ref(session, os.path.join(dataset_dir, file), dataset)
+            update_genomic_ref(session, os.path.join(dataset_dir, file))
             print(f'Processed reference set {file} for species {species} dataset {dataset}')
 
         reference_set_version = study_data['Reference_set_version']
@@ -63,10 +64,7 @@ def create_dataset(species, dataset):
 
             for ref in study_data['Reference_assemblies'].values():
                 ref = session.query(RefSeq).filter(RefSeq.name == ref['name']).one_or_none()
-                if ref.name == 'ighc':
-                    add_ighc_gene_level_features(session, ref, reference_features)
-                else:
-                    add_gene_level_features(session, ref, reference_features)
+                add_gene_level_features(session, ref, reference_features)
         else:
             # create a dummy reference with no coordinates or features
             save_genomic_ref_seq(session, 'dummy', 'dummy', '', 'dummy', 0, 0, '+')
@@ -74,12 +72,8 @@ def create_dataset(species, dataset):
             reference_features = {}
 
         session.commit()
-
         for study_name, study in study_data['Studies'].items():
             process_study(dataset_dir, reference_features, session, study, study_name, reference_set_version)
-
-        add_alias_sets(os.path.join(dataset_dir, 'alias_sets'), session)
-        session.commit()
 
     except ImportException as e:
         print(e)
@@ -195,7 +189,6 @@ def process_study(dataset_dir, reference_features, session, study, study_name, r
 
         if 'vdjbase_name' in row:
             sample_name = row['vdjbase_name']
-            print(f'using vdjbase_name from metadata: {sample_name}')
         else:
             sample_name = f'{subject_object.patient_name}_S{subject_samples[row["subject_id"]]}'
 
@@ -205,7 +198,7 @@ def process_study(dataset_dir, reference_features, session, study, study_name, r
         sample_obj = create_sample(session, study_obj, subject_object, sample_name, tissuepro_object, seqprotocol_object, datapro_object, row, annotation_method, annotation_reference)
 
         process_genomic_record(session, dataset_dir, sample_obj, study['annotation_file'], reference_features, study['bam_dir'])
-
+        
     session.commit()
     calculate_appearances(session)
     calculate_max_cov_sample(session)
@@ -224,7 +217,7 @@ def create_subject(session, study_obj, subject_name, row):
     except json.JSONDecodeError:
         raise ImportException('Error - species is not a valid ontology object.')
 
-    if species_label and (not species_id or ':' not in species_id):
+    if species_label and not species_id or ':' not in species_id:
         print(f'Invalid species ID. for {species_label} id {species_id}')
 
     row['sex'] = row['sex'].upper()
@@ -236,7 +229,7 @@ def create_subject(session, study_obj, subject_name, row):
         row['sex'] = 'not collected'
     else:
         raise ImportException(f"study {study_obj.study_name} sample {row['subject_id']}: invalid sex: {row['sex']}")
-
+        
     try:
         age_min = age_max = None
         if row['age_min']:
@@ -245,7 +238,7 @@ def create_subject(session, study_obj, subject_name, row):
             age_max = float(row['age_max'])
     except ValueError:
         raise ImportException('Error - age_min and age_max must be numeric.')
-
+        
     try:
         age_unit_id = age_unit_label = None
         if row['age_unit']:
@@ -255,7 +248,7 @@ def create_subject(session, study_obj, subject_name, row):
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - age_unit is not a valid ontology object: {e}')
 
-    if age_unit_label and (not age_unit_id or ':' not in age_unit_id):
+    if (age_unit_label and not age_unit_id) or (age_unit_label and age_unit_id and ':' not in age_unit_id):
         print(f'Error - invalid age_unit ID. for {age_unit_label} id {age_unit_id}')
 
     mother_in_study = None
@@ -266,7 +259,7 @@ def create_subject(session, study_obj, subject_name, row):
 
         if len(linked_subjects) != len(link_type):
             raise ImportException('Error - linked_subjects and link_type must have the same number of entries.')
-
+            
         linked_subjects = [x.strip() for x in linked_subjects]
         link_type = [x.strip() for x in link_type]
 
@@ -285,7 +278,7 @@ def create_subject(session, study_obj, subject_name, row):
     except json.JSONDecodeError:
         raise ImportException('Error - disease_diagnosis is not a valid ontology object.')
 
-    if disease_diagnosis_label and (not disease_diagnosis_id or ':' not in disease_diagnosis_id):
+    if (disease_diagnosis_label and not disease_diagnosis_id) or (disease_diagnosis_label and disease_diagnosis_id and ':' not in disease_diagnosis_id):
         print(f'Error - invalid disease_diagnosis ID. for species {disease_diagnosis_label} id {disease_diagnosis_id}')
 
     subject_obj = Patient(
@@ -336,7 +329,7 @@ def create_study(session, study_name, required_fields, row):
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - study type is not a valid ontology object: {e}')
 
-    if study_type_label and (not study_type_id or ':' not in study_type_id):
+    if study_type_label and not study_type_id or ':' not in study_type_id:
         print(f'Error - invalid study_type ID. for {study_type_label} id {study_type_id}')
 
     try:
@@ -381,16 +374,14 @@ def find_or_create_tissuepro(session, tissuepros, row):
     tissuepro_dict = {k: v for k, v in row.items() if k in required_fields['tissuepro']}
 
     try:
-        tissuepro_dict['tissue_id'] = tissuepro_dict['tissue_label'] = None
-        if row['tissue']:
-            type_rec = json.loads(row['tissue'])
-            tissuepro_dict['tissue_id'] = type_rec['id']
-            tissuepro_dict['tissue_label'] = type_rec['label']
+        type_rec = json.loads(row['tissue'])
+        tissuepro_dict['tissue_id'] = type_rec['id']
+        tissuepro_dict['tissue_label'] = type_rec['label']
         del tissuepro_dict['tissue']
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - tissue is not a valid ontology object: {e}')
 
-    if tissuepro_dict['tissue_label'] and (not tissuepro_dict['tissue_id'] or ':' not in tissuepro_dict['tissue_id']):
+    if (tissuepro_dict['tissue_label'] and not tissuepro_dict['tissue_id']) or (tissuepro_dict['tissue_label'] and tissuepro_dict['tissue_id'] and ':' not in tissuepro_dict['tissue_id']):
         print(f"Error - invalid tissue ID. for tissue {tissuepro_dict['tissue_label']} id {tissuepro_dict['tissue_id']}")
 
     try:
@@ -403,7 +394,7 @@ def find_or_create_tissuepro(session, tissuepros, row):
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - cell_species is not a valid ontology object: {e}')
 
-    if tissuepro_dict['cell_species_label'] and (not tissuepro_dict['cell_species_id'] or ':' not in tissuepro_dict['cell_species_id']):
+    if (tissuepro_dict['cell_species_label'] and not tissuepro_dict['cell_species_id']) or (tissuepro_dict['cell_species_label'] and tissuepro_dict['cell_species_id'] and ':' not in tissuepro_dict['cell_species_id']):
         print(f"Error - invalid cell_species ID. for {tissuepro_dict['cell_species_label']} id {tissuepro_dict['cell_species_id']}")
 
     try:
@@ -416,13 +407,14 @@ def find_or_create_tissuepro(session, tissuepros, row):
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - cell_subset is not a valid ontology object: {e}')
 
-    if tissuepro_dict['cell_subset_label'] and (not tissuepro_dict['cell_subset_id'] or ':' not in tissuepro_dict['cell_subset_id']):
+    if (tissuepro_dict['cell_subset_label'] and not tissuepro_dict['cell_subset_id']) or (tissuepro_dict['cell_subset_label'] and tissuepro_dict['cell_subset_id'] and ':' not in tissuepro_dict['cell_subset_id']):
         print(f"Error - invalid cell_subset ID. for {tissuepro_dict['cell_subset_label']} id {tissuepro_dict['cell_subset_id']}")
+
 
     for obj, td in tissuepros:
         if td == tissuepro_dict:
             return obj
-
+        
     tp_label = f"TP{len(tissuepros) + 1}"
 
     tissuepro_obj = TissuePro(
@@ -462,12 +454,14 @@ def find_or_create_seqprotocol(session, seqprotocols, row):
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - template_amount_unit is not a valid ontology object: {e}')
 
-    if seqprotocol_dict['template_amount_unit_label'] and (not seqprotocol_dict['template_amount_unit_id'] or ':' not in seqprotocol_dict['template_amount_unit_id']):
+    if (seqprotocol_dict['template_amount_unit_label'] and not seqprotocol_dict['template_amount_unit_id']) or (seqprotocol_dict['template_amount_unit_label'] and seqprotocol_dict['template_amount_unit_id'] and ':' not in seqprotocol_dict['template_amount_unit_id']):
         print(f"Error - invalid template_amount ID. for {seqprotocol_dict['template_amount_unit_label']} id {seqprotocol_dict['template_amount_unit_id']}")
 
     for obj, sd in seqprotocols:
         if sd == seqprotocol_dict:
             return obj
+
+    sp_label = f"SP{len(seqprotocols) + 1}"
 
     seqprotocol_obj = SeqProtocol(
         template_class=seqprotocol_dict['template_class'],
@@ -540,7 +534,7 @@ def create_sample(session, study, patient, sample_name, tissuepro, seqprotocol, 
     except json.JSONDecodeError as e:
         raise ImportException(f'Error - template_amount_unit is not a valid ontology object: {e}')
 
-    if row['collection_time_point_relative_unit_label'] and (not row['collection_time_point_relative_unit_id'] or ':' not in row['collection_time_point_relative_unit_id']):
+    if (row['collection_time_point_relative_unit_label'] and not row['collection_time_point_relative_unit_id']) or (row['collection_time_point_relative_unit_label'] and row['collection_time_point_relative_unit_id'] and ':' not in row['collection_time_point_relative_unit_id']):
         print(f"Error - invalid collection_time_point_relative_unit ID. for  {row['collection_time_point_relative_unit_label']} id {row['collection_time_point_relative_unit_id']}")
 
     ref_id = None
@@ -580,3 +574,4 @@ def create_sample(session, study, patient, sample_name, tissuepro, seqprotocol, 
     session.add(sample_obj)
     session.commit()
     return sample_obj
+    
